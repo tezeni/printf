acf0b2b05ecd3dfe581b27551853bc21ed4b664d
tezeni <teztig@gmail.com>
2023-05-29 05:30:50 -0700

    main.h

---
+++ README.md
@@ -0,0 +1,1 @@
+readme for the printf team project
---
+++ test/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+#include <stdarg.h>
+
+#include <stdio.h>
+
+#include <unistd.h>
+
+
+int _printf(const char *format, ...);
+
+#endif
